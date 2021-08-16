2848aa35d0f4b61e88ee1adc7869a562f940cbd6
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2021-08-16 20:15:18 +0200

    maj

---
+++ ios/DerivedData/73B7CBD7-9753-4DD5-8753-01050AECA23B/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Capacitor.build/Objects-normal/arm64/Capacitor_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/pierrefrancoispaoletti/appdevelopment/1755-resas/ios/DerivedData/73B7CBD7-9753-4DD5-8753-01050AECA23B/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Capacitor.build/DerivedSources/Capacitor_vers.c
---
+++ ios/DerivedData/73B7CBD7-9753-4DD5-8753-01050AECA23B/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/CapacitorCordova.build/Objects-normal/arm64/AppDelegate.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/pierrefrancoispaoletti/appdevelopment/1755-resas/node_modules/@capacitor/ios/CapacitorCordova/CapacitorCordova/Classes/Public/AppDelegate.m \
+  /Users/pierrefrancoispaoletti/appdevelopment/1755-resas/ios/DerivedData/73B7CBD7-9753-4DD5-8753-01050AECA23B/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/CapacitorCordova.build/module.modulemap \
+  /Users/pierrefrancoispaoletti/appdevelopment/1755-resas/ios/App/Pods/Target\ Support\ Files/CapacitorCordova/CapacitorCordova-prefix.pch \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/dyld.modulemap \
+  /Users/pierrefrancoispaoletti/appdevelopment/1755-resas/node_modules/@capacitor/ios/CapacitorCordova/CapacitorCordova/Classes/Public/AppDelegate.h \
+  /Users/pierrefrancoispaoletti/appdevelopment/1755-resas/node_modules/@capacitor/ios/CapacitorCordova/CapacitorCordova/Classes/Public/CDVViewController.h
